9ac3526c9e5c828cfd5ed1b75ac13e7cdfdd2a9f
danieldorazio <danieldorazio.97@gmail.com>
2023-11-15 15:09:03 +0100

    add: methods: addTodo

--- js/script.js
+++ js/script.js
@@ -6,14 +6,21 @@ createApp ({
             todoList: [
                 "lavare",
                 "pulire",
-                "fare la spesa"
+                "fare la spesa",
             ],
+            newTodoList: "",
         }
     },
 
     methods: {
+        // FUNZIONE CHE CANCELLA DALL'ARRAY L'ELEMENTO CLICCATO
         clickDeleteTodo: function(clickedIndex) {
            this.todoList.splice(clickedIndex, 1);
+        },
+        // FUNZIONE CHE INSERISCE UNATRINGA ALL'INTERNO DI UN ARRAY E AZZERA QUELLA STRINGA 
+        addTodo: function() {
+            this.todoList.push(this.newTodoList);
+            this.newTodoList = "";
         }
     }
 }).mount("#app");
